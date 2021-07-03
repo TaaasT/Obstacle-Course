0f3c98781b706551f93a18a520f13954a3ec5226
TaaasT <61253797+TaaasT@users.noreply.github.com>
2021-07-03 19:50:08 +0200

    8-13
    Basic movement on player.

---
+++ Obstacle Course/Assets/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal");
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue, 0, zValue);
+
+    }
+
+}
