da0cb18bf43cf476249eaf25abe4ace9d566d5c4
TaaasT <61253797+TaaasT@users.noreply.github.com>
2021-07-04 13:34:43 +0200

    14-18
    Cinemachine, basic controls and refactoring

--- Obstacle Course/Assets/Scripts/Mover.cs
+++ Obstacle Course/Assets/Scripts/Mover.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
 
     void Start()
     {
-
+        PrintInstructions();
     }
 
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
-        float zValue = Input.GetAxis("Vertical");
-        transform.Translate(xValue, 0, zValue);
+        MovePlayer();
+    }
 
+    void PrintInstructions()
+    {
+        Debug.Log("welcome to the game!");
+        Debug.Log("Use WASD to move your character");
+        Debug.Log("Don't hit the walls!!");
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue, 0, zValue);
     }
 
 }
